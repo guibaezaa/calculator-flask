fc0a7a0f080fbf105c382b5eeb6b3a13f0474ce5
Guilherme Baeza <guibaezaa@live.com>
2025-12-22 17:13:13 +0000

    create the rule for first calculator

--- src/main/routes/calculators.py
+++ src/main/routes/calculators.py
@@ -1,8 +1,15 @@
 from flask import Blueprint, jsonify, request
+from src.calculators.calculator_1 import Calculator1
 
 calc_route_bp = Blueprint('calc_routes', __name__)
 
 @calc_route_bp.route('/calculator/1', methods=['POST'])
 def calculator_1():
-   print(request.json)
-   return jsonify({"sucess": True}), 200
+   calc = Calculator1()
+   try:
+      response = calc.calculate(request)
+      return jsonify(response), 200
+   except ValueError as e:
+      return jsonify({"error": str(e)}), 400
+   except Exception:
+      return jsonify({"error": "Internal server error"}), 500
